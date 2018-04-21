87837c16014fe0057f137d6f1b9591e0a77c82ec
Adrian Danis <rambobones@gmail.com>
2018-04-21 16:46:02 +1000

    Print out the properties

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,30 @@ mod intrinsics;
 pub use panic::rust_begin_panic;
 pub use intrinsics::*;
 
+struct GUIDWrap(uefi::Guid);
+impl core::fmt::Display for GUIDWrap {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        let v = match self.0 {
+            uefi::table::cfg::ACPI_GUID => Some("ACPI"),
+            uefi::table::cfg::ACPI2_GUID => Some("ACPI2"),
+            uefi::table::cfg::PROPERTIES_TABLE_GUID => Some("UEFI Properties"),
+            uefi::table::cfg::DEBUG_IMAGE_INFO_GUID => Some("Debug image info"),
+            _ => None,
+        };
+        match v {
+            None => write!(f, "unknown GUID {}", self.0),
+            Some(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+struct CTEWrap(&'static uefi::table::cfg::ConfigTableEntry);
+impl core::fmt::Display for CTEWrap {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "{} at {:p}", GUIDWrap(self.0.guid), self.0.address as *mut usize)
+    }
+}
+
 pub static mut UEFI_SYSTEM_TABLE: Option<&'static table::SystemTable> = None;
 
 #[no_mangle]
@@ -29,12 +53,16 @@ pub extern "win64" fn UefiMain(_handle: Handle, st: &'static table::SystemTable)
         UEFI_SYSTEM_TABLE = Some(&st);
     }
     panic::set_panic_action(panic::Action::ShutdownDelay(core::time::Duration::from_secs(1)));
-    loader_main();
+    loader_main(st);
     Status::Success
 }
 
-fn loader_main() -> () {
+fn loader_main(st: &'static table::SystemTable) -> () {
     println!("Hello world!");
+    println!("Have configuration values:");
+    for config in st.config_table() {
+        println!("\t{}", CTEWrap(config));
+    }
     unimplemented!();
 }
 
