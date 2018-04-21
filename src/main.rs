322043690ee3df3625ee95355864f9ba31c84ac8
Adrian Danis <rambobones@gmail.com>
2018-04-21 16:45:59 +1000

    configurable panic actions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(lang_items)]
 #![feature(compiler_builtins_lib)]
+#![feature(duration_extras)]
 #![no_std]
 #![no_main]
 
@@ -7,6 +8,7 @@ extern crate rlibc;
 extern crate compiler_builtins;
 
 extern crate uefi;
+extern crate fencedvar;
 
 use uefi::{Handle, Status};
 use uefi::table;
@@ -26,6 +28,7 @@ pub extern "win64" fn UefiMain(_handle: Handle, st: &'static table::SystemTable)
     unsafe {
         UEFI_SYSTEM_TABLE = Some(&st);
     }
+    panic::set_panic_action(panic::Action::ShutdownDelay(core::time::Duration::from_secs(1)));
     loader_main();
     Status::Success
 }
--- src/panic.rs
+++ src/panic.rs
@@ -1,5 +1,23 @@
 use core;
+use core::time::Duration;
 use uefi;
+use fencedvar::FencedVar;
+
+#[derive(Debug, Clone, Copy)]
+#[allow(dead_code)]
+pub enum Action {
+    Spin,
+    Restart,
+    RestartDelay(Duration),
+    Shutdown,
+    ShutdownDelay(Duration),
+}
+
+static mut PANIC_ACTION: FencedVar<Action> = FencedVar{ construction_value: Action::Shutdown };
+
+pub fn set_panic_action(action: Action) -> () {
+    unsafe {PANIC_ACTION.set(action)}
+}
 
 #[lang = "panic_fmt"]
 #[no_mangle]
@@ -8,9 +26,27 @@ pub extern fn rust_begin_panic(msg: core::fmt::Arguments,
                                line: u32,
                                column: u32) -> ! {
     println!("Panic at {} {}:{}: {}", file, line, column, msg);
-    println!("Powering down in 5 seconds...");
-    let boottime = unsafe {::UEFI_SYSTEM_TABLE.unwrap().boot};
-    boottime.stall(5000000);
-    let runtime = unsafe {::UEFI_SYSTEM_TABLE.unwrap().runtime};
-    runtime.reset(uefi::table::runtime::ResetType::Shutdown, uefi::Status::Aborted, None)
+    let table = || { unsafe {::UEFI_SYSTEM_TABLE.unwrap() } };
+    let boot = || { table().boot };
+    let run = || { table().runtime };
+    let to_micros = |x: Duration| { x.as_secs() * 1000000 + x.subsec_micros() as u64 };
+    let sleep = |x: Duration| { boot().stall(to_micros(x) as usize) };
+    let reset = |reset_type| { run().reset(reset_type, uefi::Status::Aborted, None) };
+    let restart = || { reset(uefi::table::runtime::ResetType::Warm) };
+    let shutdown = || { reset(uefi::table::runtime::ResetType::Shutdown) };
+    match unsafe {PANIC_ACTION.get()} {
+        Action::Spin => loop {},
+        Action::Restart => restart(),
+        Action::RestartDelay(duration) => {
+            println!("Restarting in {} seconds...", duration.as_secs());
+            sleep(duration);
+            restart()
+        },
+        Action::Shutdown => shutdown(),
+        Action::ShutdownDelay(duration) => {
+            println!("Powering down in {} seconds...", duration.as_secs());
+            sleep(duration);
+            shutdown()
+        },
+    }
 }
